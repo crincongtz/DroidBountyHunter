814fb40ca6b849be1698a2a258b6969a5cef71a4
Alejandro Rincon <crincon@itexico.net>
2019-07-04 23:20:23 -0500

    Laboratorio 05 terminado

--- app/src/main/java/edu/training/droidbountyhunter/models/Fugitive.java
+++ app/src/main/java/edu/training/droidbountyhunter/models/Fugitive.java
@@ -8,12 +8,16 @@ public class Fugitive implements Parcelable {
     private String name;
     private String status;
     private String photo;
+    private double latitude;
+    private double longitude;
 
     public Fugitive (int id, String name, String status) {
         this.id = id;
         this.name = name;
         this.status = status;
         this.photo = "";
+        this.latitude = 0d;
+        this.longitude = 0d;
     }
 
     public Fugitive(int id, String name, String status, String photo) {
@@ -21,6 +25,17 @@ public class Fugitive implements Parcelable {
         this.name = name;
         this.status = status;
         this.photo = photo;
+        this.latitude = 0d;
+        this.longitude = 0d;
+    }
+
+    public Fugitive(int id, String name, String status, String photo, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.photo = photo;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     protected Fugitive(Parcel in) {
@@ -28,6 +43,8 @@ public class Fugitive implements Parcelable {
         name = in.readString();
         status = in.readString();
         photo = in.readString();
+        latitude = in.readDouble();
+        longitude = in.readDouble();
     }
 
     public static final Creator<Fugitive> CREATOR = new Creator<Fugitive>() {
@@ -74,6 +91,22 @@ public class Fugitive implements Parcelable {
         this.photo = photo;
     }
 
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -85,6 +118,8 @@ public class Fugitive implements Parcelable {
         dest.writeString(name);
         dest.writeString(status);
         dest.writeString(photo);
+        dest.writeDouble(latitude);
+        dest.writeDouble(longitude);
     }
 }
 
