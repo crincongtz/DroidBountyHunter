5c2cd600b379c7ccb1e55db1b905c48970019631
Alejandro Rincón <crincongtz@gmail.com>
2020-10-15 15:13:09 -0500

    Update to Android X

--- app/src/main/java/edu/training/droidbountyhunter/HomeActivity.java
+++ app/src/main/java/edu/training/droidbountyhunter/HomeActivity.java
@@ -1,16 +1,15 @@
 package edu.training.droidbountyhunter;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 import edu.training.droidbountyhunter.ui.home.SectionsPagerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
